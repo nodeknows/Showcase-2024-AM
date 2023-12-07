d0f9ea76e9234fd1c79b4d56876358bdd0d14f32
Asian Himothy <evan.yang23@student.cart.org>
2023-12-07 10:44:05 -0800

    .

--- js/index.js
+++ js/index.js
@@ -1,25 +1,43 @@
 const nav = document.getElementsByClassName('nav');
-const left = document.getElementById('left')
-const right = document.getElementById('right')
+const left = document.getElementById('left');
+const right = document.getElementById('right');
 
-const screens = document.getElementsByClassName('screens')
+const screens = document.getElementsByClassName('screens');
 
-const screen1 = document.getElementById('screen1')
-const screen2 = document.getElementById('screen2')
-const screen3 = document.getElementById('screen3')
+const s1 = document.getElementById('s1');
+const s2 = document.getElementById('s2');
+const s3 = document.getElementById('s3');
 
-left.onmousedown = function() {
-    console.log('left');
-
-    var screenGoal = $("screen" + `screens.dataset.currentScreen`);
-
-    console.log(screenGoal)
-
-    screen1.animate({
-        transform: `translate(-100%, 0)`
-    }, {duration: .5, fill: 'forwards'})
-};
+var currentScreen = 1;
 
 right.onmousedown = function() {
     console.log('right');
+
+    var screenGoal = currentScreen+1;
+
+    console.log(screenGoal)
+    
+    if (screenGoal >= 2 && screenGoal != 4) {
+        currentScreen += 1
+        s1.animate({
+            transform: `translate(-100vw, 0)`
+        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
+        s2.animate({
+            transform: `translate(-100vw, 0)`
+        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
+        s3.animate({
+            transform: `translate(-100vw, 0)`
+        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
+    }
+
+    if (screenGoal == 4) {
+        currentScreen = 1
+        s1.animate({
+            transform: `translate(-100vw, 0)`
+        }, {duration: 800, fill: 'forwards', easing: "ease-in"})
+    }
+};
+
+left.onmousedown = function() {
+    console.log('left');
 };
