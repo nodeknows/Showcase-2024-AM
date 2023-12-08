3ae444f1e67132e52779e1064530d7b88c16323a
SmartNode <evan.yang23@student.cart.org>
2023-12-07 20:26:21 -0800

    .

--- js/index.js
+++ js/index.js
@@ -1,57 +1,84 @@
-const left = document.getElementById('left');
-const right = document.getElementById('right');
+var right = document.getElementById('right');
+var left = document.getElementById('left');
 
-const s1 = document.getElementById('s1');
-const s2 = document.getElementById('s2');
-const s3 = document.getElementById('s3');
+var s1 = document.getElementById('s1');
+var s2 = document.getElementById('s2');
+var s3 = document.getElementById('s3');
 
 var currentScreen = 1;
 var animating = false;
+var currentTranslateX = 0; // Store the current translated position
 
 right.onmousedown = function () {
     console.log('right');
 
     var screenGoal = currentScreen + 1;
 
-    console.log(screenGoal)
+    console.log(screenGoal);
 
-    if (screenGoal >= 2 && screenGoal != 4) {
-        if (!(animating == true)) {
-            currentScreen += 1
+    if (screenGoal >= 2 && screenGoal !== 4) {
+        if (!animating) {
+            currentScreen += 1;
 
             animating = true;
-            
-            s1.animate({
-                transform: `translate(-100vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            s2.animate({
-                transform: `translate(-100vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            s3.animate({
-                transform: `translate(-100vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            setTimeout(function(){
+
+            s1.animate(
+                {
+                    transform: `translate(${currentTranslateX - 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s2.animate(
+                {
+                    transform: `translate(${currentTranslateX - 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s3.animate(
+                {
+                    transform: `translate(${currentTranslateX - 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+
+            // Update the current translated position for the next animation
+            currentTranslateX -= 100;
+
+            setTimeout(function () {
                 animating = false;
             }, 800);
         }
     }
 
-    if (screenGoal == 4) {
-        if (!(animating == true)) {
-            currentScreen = 1
+    if (screenGoal === 4) {
+        if (!animating) {
+            currentScreen = 1;
 
             animating = true;
 
-            s1.animate({
-                transform: `translate(300vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            s2.animate({
-                transform: `translate(300vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            s3.animate({
-                transform: `translate(300vw, 0)`
-            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
-            setTimeout(function(){
+            s1.animate(
+                {
+                    transform: `translate(${currentTranslateX + 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s2.animate(
+                {
+                    transform: `translate(${currentTranslateX + 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s3.animate(
+                {
+                    transform: `translate(${currentTranslateX + 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+
+            // Update the current translated position for the next animation
+            currentTranslateX += 200;
+
+            setTimeout(function () {
                 animating = false;
             }, 800);
         }
@@ -60,4 +87,76 @@ right.onmousedown = function () {
 
 left.onmousedown = function () {
     console.log('left');
+
+    var screenGoal = currentScreen - 1;
+
+    console.log(screenGoal);
+
+    if (screenGoal >= 1 && screenGoal !== 0) {
+        if (!animating) {
+            currentScreen -= 1;
+
+            animating = true;
+
+            s1.animate(
+                {
+                    transform: `translate(${currentTranslateX + 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s2.animate(
+                {
+                    transform: `translate(${currentTranslateX + 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s3.animate(
+                {
+                    transform: `translate(${currentTranslateX + 100}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+
+            // Update the current translated position for the next animation
+            currentTranslateX += 100;
+
+            setTimeout(function () {
+                animating = false;
+            }, 800);
+        }
+    }
+
+    if (screenGoal === 0) {
+        if (!animating) {
+            currentScreen = 3;
+
+            animating = true;
+
+            s1.animate(
+                {
+                    transform: `translate(${currentTranslateX - 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s2.animate(
+                {
+                    transform: `translate(${currentTranslateX - 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+            s3.animate(
+                {
+                    transform: `translate(${currentTranslateX - 200}vw, 0)`
+                },
+                { duration: 800, fill: 'forwards', easing: 'ease-out' }
+            );
+
+            // Update the current translated position for the next animation
+            currentTranslateX -= 200;
+
+            setTimeout(function () {
+                animating = false;
+            }, 800);
+        }
+    }
 };
