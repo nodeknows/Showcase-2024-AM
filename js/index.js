9fdedbc17ee24d66f51a0506e4ffff70bb3aaeee
SmartNode <evan.yang23@student.cart.org>
2023-12-07 20:17:34 -0800

    ,

--- js/index.js
+++ js/index.js
@@ -1,43 +1,63 @@
-const nav = document.getElementsByClassName('nav');
 const left = document.getElementById('left');
 const right = document.getElementById('right');
-
-const screens = document.getElementsByClassName('screens');
 
 const s1 = document.getElementById('s1');
 const s2 = document.getElementById('s2');
 const s3 = document.getElementById('s3');
 
 var currentScreen = 1;
+var animating = false;
 
-right.onmousedown = function() {
+right.onmousedown = function () {
     console.log('right');
 
-    var screenGoal = currentScreen+1;
+    var screenGoal = currentScreen + 1;
 
     console.log(screenGoal)
-    
+
     if (screenGoal >= 2 && screenGoal != 4) {
-        currentScreen += 1
-        s1.animate({
-            transform: `translate(-100vw, 0)`
-        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
-        s2.animate({
-            transform: `translate(-100vw, 0)`
-        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
-        s3.animate({
-            transform: `translate(-100vw, 0)`
-        }, {duration: 800, fill: 'forwards', easing: "ease-out"})
+        if (!(animating == true)) {
+            currentScreen += 1
+
+            animating = true;
+            
+            s1.animate({
+                transform: `translate(-100vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            s2.animate({
+                transform: `translate(-100vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            s3.animate({
+                transform: `translate(-100vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            setTimeout(function(){
+                animating = false;
+            }, 800);
+        }
     }
 
     if (screenGoal == 4) {
-        currentScreen = 1
-        s1.animate({
-            transform: `translate(-100vw, 0)`
-        }, {duration: 800, fill: 'forwards', easing: "ease-in"})
+        if (!(animating == true)) {
+            currentScreen = 1
+
+            animating = true;
+
+            s1.animate({
+                transform: `translate(300vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            s2.animate({
+                transform: `translate(300vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            s3.animate({
+                transform: `translate(300vw, 0)`
+            }, { duration: 800, fill: 'forwards', easing: "ease-out" })
+            setTimeout(function(){
+                animating = false;
+            }, 800);
+        }
     }
 };
 
-left.onmousedown = function() {
+left.onmousedown = function () {
     console.log('left');
 };
